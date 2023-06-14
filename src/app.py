14b91be529143d36e5b0a81b4a52ee554702b14b
bits-io <bits1933@gmail.com>
2023-06-14 19:03:11 +0700

    add flask

--- src/app.py
+++ src/app.py
@@ -12,15 +12,15 @@ def recommend_places(activity: str = "Outdoor", category: str = "Beach", latitud
 
 @app.route('/hotel_recommendation', methods=['POST'])
 def recommend_hotels(activity: str = "Outdoor", category: str = "Beach", latitude: float = -1.212555, longitude: float = 116.98106):
-    return recommend_hotels(activity, category, latitude, longitude, 3)
+    return recommend_hotels(latitude, longitude)
 
 @app.route('/restaurant_recommendation', methods=['POST'])
 def recommend_restaurants(activity: str = "Outdoor", category: str = "Beach", latitude: float = -1.212555, longitude: float = 116.98106):
-    return recommend_restaurants(activity, category, latitude, longitude, 3)
+    return recommend_restaurants(latitude, longitude)
 
 @app.route('/itinerary_recommendation', methods=['POST'])
 def itinerary(activity: str = "Outdoor", category: str = "Beach", latitude: float = -1.212555, longitude: float = 116.98106):
-    return itinerary_left(activity, category, latitude, longitude, 3)
+    return itinerary_left(activity, category, latitude, longitude)
 
 if __name__ == '__main__':
     app.run()
